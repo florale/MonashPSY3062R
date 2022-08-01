c96b73b00a83d46b61e80e99fc13c66e28216d9f
Flora Le <flora.le@monash.edu>
2022-08-01 11:46:34 +1000

    Week 2 materials
    minor edit to student's rmd

---
+++ Week2_GetStarted.R
@@ -0,0 +1,17 @@
+## Getting started in R
+
+# this is an R script (.R)
+# we use "hashtags" in R as comments or annotations 
+# R won't run any lines that begin with a hashtag
+
+1 + 1
+# 2 + 2
+
+## Which line does R run?
+
+# Several ways to run a code:
+# 1. Choose the lines you'd like to run
+# 1.1. For a single line of code (e.g., line 7), click on the line you intend to run
+# 1.2. For multiple lines (e.g., line 32 to 38), highlight the lines that you intend to run
+# 2. Click "Run" at the top right corner of the script
+# 2. Click CTRL+ENTER (for Windows) or CMD+ENTER (for MACs)
